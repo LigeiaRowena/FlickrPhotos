504b53a4677dc7ee80621889d0e2e60dedc22485
Francesca Corsini <francesca.corsini@wuerth-phoenix.com>
2016-12-21 15:02:56 +0100

    Initial Commit

---
+++ FlickrPhotos/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FlickrPhotos
+//
+//  Created by Francesca Corsini on 21/12/16.
+//  Copyright © 2016 Francesca Corsini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
